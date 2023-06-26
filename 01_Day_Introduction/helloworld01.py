55964e29c7b01fe72c5a654d29540178034ffaf7
JokerFL <17707968914@163.com>
2023-06-26 09:20:13 +0800

    python常见的一些基础类型

---
+++ 01_Day_Introduction/helloworld01.py
@@ -0,0 +1,16 @@
+print(2+3)
+print(3-1)
+print(3*4)
+print(9/3)
+print(3**2)  #幂级运算
+print(5%2)   #取余
+print(5//2)  #整数整除
+
+print(type(10))
+print(type(3.14))
+print(type(1+3j))
+print(type('hello'))
+print(type([1,2,3]))  #列表 list
+print(type({'name':'hei'}))
+print(type({9.8,3.14,2.7}))  #集合 set
+print(type((9.8,3.14,2.7)))  #元组 tuple 90 135
